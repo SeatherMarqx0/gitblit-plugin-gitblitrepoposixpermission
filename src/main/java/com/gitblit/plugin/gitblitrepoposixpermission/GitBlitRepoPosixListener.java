b142657248f83ea50e758163d114326579265c2e
William Cork <wcork@lbl.gov>
2015-07-21 16:50:50 -0700

    Google Inject removed. Plugin now works with static Managers. Repos are iterated via Manager rather than directory structure (no recursion).

--- src/main/java/com/gitblit/plugin/gitblitrepoposixpermission/GitBlitRepoPosixListener.java
+++ src/main/java/com/gitblit/plugin/gitblitrepoposixpermission/GitBlitRepoPosixListener.java
@@ -6,16 +6,13 @@ import com.gitblit.manager.IRepositoryManager;
 import com.gitblit.manager.IRuntimeManager;
 import com.gitblit.manager.IUserManager;
 import com.gitblit.models.TeamModel;
-//import com.gitblit.servlet.GitblitContext;
-import com.google.inject.Inject;
-import org.eclipse.jgit.internal.storage.file.FileRepository;
+import com.gitblit.servlet.GitblitContext;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.StoredConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ro.fortsoft.pf4j.Extension;
 
-//import javax.inject.Inject;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -37,19 +34,17 @@ public class GitBlitRepoPosixListener extends LifeCycleListener
   private static final Properties prop = new Properties();
 
   final Logger log = LoggerFactory.getLogger(getClass());
-  final IRuntimeManager runtimeManager;
-  final IRepositoryManager repositoryManager;
-  final IUserManager userManager;
+  private IRuntimeManager runtimeManager = null;
+  private IRepositoryManager repositoryManager = null;
+  private IUserManager userManager = null;
 
-  //TODO: This is possibly where the error occurs. No idea what's going on here...
-  @Inject
-  public GitBlitRepoPosixListener(IRuntimeManager runtimeManager,
-                                  IRepositoryManager repositoryManager,
-                                  IUserManager userManager)
+  public GitBlitRepoPosixListener()
   {
-    this.runtimeManager = runtimeManager;
-    this.repositoryManager = repositoryManager;
-    this.userManager = userManager;
+    super();
+    //I guess this needs to be here?
+    this.runtimeManager = GitblitContext.getManager(IRuntimeManager.class);
+    this.repositoryManager = GitblitContext.getManager(IRepositoryManager.class);
+    this.userManager = GitblitContext.getManager(IUserManager.class);
   }
 
   @Override
@@ -62,7 +57,7 @@ public class GitBlitRepoPosixListener extends LifeCycleListener
         //Get properties
         prop.load(new FileInputStream("GitBlitRepoPosix.properties"));
         //Modify repos to owner:group
-        modDirectoryRecursive(runtimeManager.getBaseFolder());
+        modAllReposByString();
         log.info(this.getClass().getName() + ": Completed posix repository permission settings");
       } catch (IOException e)
       {
@@ -83,82 +78,77 @@ public class GitBlitRepoPosixListener extends LifeCycleListener
   }
 
   /**
-   * Helper function to traverse a given folder recursively.
+   * Helper function to set all repo permissions.
    *
-   * @param currentDir the starting directory.
    * @throws IOException from File errors
    */
-  private void modDirectoryRecursive(File currentDir) throws IOException
+  private void modAllReposByString() throws IOException
   {
-    //Recursive search for *.git dir
-    File[] currentFileNames = currentDir.listFiles();
-    if (currentFileNames == null) return; // just a file
-
-    if (currentDir.isDirectory())
+    for(String currentRepName : repositoryManager.getRepositoryList())
     {
-      for (File file : currentFileNames)
+      Repository localRepo = repositoryManager.getRepository(currentRepName);
+      File currentFolder = localRepo.getDirectory();
+      log.debug(this.getClass().getName() + ": Found a repo: " + currentRepName);
+      PosixFileAttributes attrs = Files.getFileAttributeView(currentFolder.toPath(),
+              PosixFileAttributeView.class).readAttributes();
+      log.debug(this.getClass().getName() + ": Configuring owner and group to: " +
+              attrs.owner() + ":" + attrs.group());
+
+      StoredConfig localConfig = localRepo.getConfig();
+
+      //Configs given from GitBlitRepoPosix.properties
+      localConfig.setString("gitblit", null, "owner", attrs.owner().getName());
+      localConfig.setBoolean("gitblit", null, "acceptnewpatchsets",
+              Boolean.parseBoolean(prop.getProperty("gitblit.acceptnewpatchsets")));
+      localConfig.setBoolean("gitblit", null, "acceptnewtickets",
+              Boolean.parseBoolean(prop.getProperty("gitblit.acceptnewtickets")));
+      localConfig.setBoolean("gitblit", null, "useincrementalpushtags",
+              Boolean.parseBoolean(prop.getProperty("gitblit.useincrementalpushtags")));
+      localConfig.setBoolean("gitblit", null, "allowforks",
+              Boolean.parseBoolean(prop.getProperty("gitblit.allowforks")));
+      localConfig.setString("gitblit", null, "accessrestriction",
+              prop.getProperty("gitblit.accessrestriction"));
+      localConfig.setString("gitblit", null, "authorizationcontrol",
+              prop.getProperty("gitblit.authorizationcontrol"));
+      localConfig.setBoolean("gitblit", null, "verifycommitter",
+              Boolean.parseBoolean(prop.getProperty("gitblit.verifycommitter")));
+      localConfig.setBoolean("gitblit", null, "showremotebranches",
+              Boolean.parseBoolean(prop.getProperty("gitblit.showremotebranches")));
+      localConfig.setBoolean("gitblit", null, "isfrozen",
+              Boolean.parseBoolean(prop.getProperty("gitblit.isfrozen")));
+      localConfig.setBoolean("gitblit", null, "skipsizecalculation",
+              Boolean.parseBoolean(prop.getProperty("gitblit.skipsizecalculation")));
+      localConfig.setBoolean("gitblit", null, "skipsummarymetrics",
+              Boolean.parseBoolean(prop.getProperty("gitblit.skipsummarymetrics")));
+      localConfig.setString("gitblit", null, "federationstrategy",
+              prop.getProperty("gitblit.federationstrategy"));
+      localConfig.setBoolean("gitblit", null, "isfederated",
+              Boolean.parseBoolean(prop.getProperty("gitblit.isfederated")));
+      localConfig.setString("gitblit", null, "gcthreshold",
+              prop.getProperty("gitblit.gcthreshold"));
+      localConfig.setString("gitblit", null, "lastgc",
+              prop.getProperty("gitblit.lastgc"));
+
+      //Save and close repo
+      localConfig.save();
+      localRepo.close();
+
+      //Add repo to group(attrs.group().getName()) in users.conf via java API
+      log.info(String.format("Finding TeamModel for group=%s: %s",
+              attrs.group().getName(), userManager.getTeamModel(attrs.group().getName())));
+      TeamModel localTeam = userManager.getTeamModel(attrs.group().getName());
+      if(localTeam != null)
       {
-        if (file.getName().matches(".*\\.git")) //Found a git repo
-        {
-          //Add configs.
-          PosixFileAttributes attrs = Files.getFileAttributeView(file.toPath(),
-                  PosixFileAttributeView.class).readAttributes();
-          log.debug(this.getClass().getName() + ": Found a repo: " + file.getAbsolutePath());
-          Repository localRepo = new FileRepository(file.getPath());
-          StoredConfig localConfig = localRepo.getConfig();
-
-          log.debug(this.getClass().getName() + ": Configuring owner and group to: " +
-                  attrs.owner() + ":" + attrs.group());
-
-          //Configs given from GitBlitRepoPosix.properties
-          localConfig.setString("gitblit", null, "owner", attrs.owner().getName());
-          localConfig.setBoolean("gitblit", null, "acceptnewpatchsets",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.acceptnewpatchsets")));
-          localConfig.setBoolean("gitblit", null, "acceptnewtickets",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.acceptnewtickets")));
-          localConfig.setBoolean("gitblit", null, "useincrementalpushtags",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.useincrementalpushtags")));
-          localConfig.setBoolean("gitblit", null, "allowforks",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.allowforks")));
-          localConfig.setString("gitblit", null, "accessrestriction",
-                  prop.getProperty("gitblit.accessrestriction"));
-          localConfig.setString("gitblit", null, "authorizationcontrol",
-                  prop.getProperty("gitblit.authorizationcontrol"));
-          localConfig.setBoolean("gitblit", null, "verifycommitter",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.verifycommitter")));
-          localConfig.setBoolean("gitblit", null, "showremotebranches",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.showremotebranches")));
-          localConfig.setBoolean("gitblit", null, "isfrozen",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.isfrozen")));
-          localConfig.setBoolean("gitblit", null, "skipsizecalculation",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.skipsizecalculation")));
-          localConfig.setBoolean("gitblit", null, "skipsummarymetrics",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.skipsummarymetrics")));
-          localConfig.setString("gitblit", null, "federationstrategy",
-                  prop.getProperty("gitblit.federationstrategy"));
-          localConfig.setBoolean("gitblit", null, "isfederated",
-                  Boolean.parseBoolean(prop.getProperty("gitblit.isfederated")));
-          localConfig.setString("gitblit", null, "gcthreshold",
-                  prop.getProperty("gitblit.gcthreshold"));
-          localConfig.setString("gitblit", null, "lastgc",
-                  prop.getProperty("gitblit.lastgc"));
-
-          //Save and close repo
-          localConfig.save();
-          localRepo.close();
-
-          //Add repo to group(attrs.group().getName()) in users.conf via java API
-          TeamModel localTeam = userManager.getTeamModel(attrs.group().getName());
-          localTeam.setRepositoryPermission(file.getPath(), Constants.AccessPermission.DELETE);
-          userManager.updateTeamModel(attrs.group().getName(), localTeam);
-
-        } else
-        {
-          //Not a repo. Enter dir.
-          modDirectoryRecursive(file);
-        }
+        log.info(String.format("Adding %s permissions of repo: %s to group: %s",
+                Constants.AccessPermission.DELETE, currentRepName, attrs.group().getName()));
+        localTeam.setRepositoryPermission(currentRepName, Constants.AccessPermission.DELETE);
       }
-      //else skip
+      else
+      {
+        log.warn("TeamModel for group " + attrs.group().getName() + " does not exist.");
+      }
+      userManager.updateTeamModel(attrs.group().getName(), localTeam);
     }
   }
+
 }
